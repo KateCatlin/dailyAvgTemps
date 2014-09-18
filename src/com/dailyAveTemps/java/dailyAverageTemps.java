07911e2416197fc75f7839d33bd228207b041054
Kate Catlin <kate.catlin@detroitlabs.com>
2014-09-18 15:49:32 -0400

    Finally Creating Project

---
+++ src/com/dailyAveTemps/java/dailyAverageTemps.java
@@ -0,0 +1,11 @@
+package com.dailyAveTemps.java;
+
+/**
+ * Created by katecatlin on 9/18/14.
+ */
+public class dailyAverageTemps {
+
+    public static void main(String[] args) {
+        
+    }
+}
