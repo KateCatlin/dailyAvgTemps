e8f3b487fcb5960f50da70656ac9b7cb84531d5a
Kate Catlin <kate.catlin@detroitlabs.com>
2014-09-18 16:23:16 -0400

    adding all the code. It's... done.

--- src/com/dailyAveTemps/java/dailyAverageTemps.java
+++ src/com/dailyAveTemps/java/dailyAverageTemps.java
@@ -5,7 +5,26 @@ package com.dailyAveTemps.java;
  */
 public class dailyAverageTemps {
 
+    public static int dailyAverageTemps [] = {63, 73, 83, 93};
+
     public static void main(String[] args) {
-        
+
+
+        double weekAverage = findWeekAverage();
+        System.out.println(weekAverage);
+
+    }
+
+    public static double findWeekAverage () {
+        double sum = 0;
+        double weekAverage = 0;
+        int i = 0;
+
+        for (int x : dailyAverageTemps) {
+            sum +=x;
+        }
+        weekAverage = sum / dailyAverageTemps.length;
+
+        return weekAverage;
     }
 }
